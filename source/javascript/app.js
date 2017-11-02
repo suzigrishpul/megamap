fe73880a2458e6f8434f9dfdf953a2c998d5cdec
rapicastillo <rapi.castillo@gmail.com>
2017-11-02 11:28:32 -0400

    Finalize look and styling pre-scripting

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -4,4 +4,20 @@
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors • <a href="//350.org">350.org</a>'
   }).addTo(map);
+
+
+  // 1. google maps geocode
+
+  // 2. focus map on geocode (via lat/lng)
+
+  // 3. markers on map
+
+  // 4. filter out items in activity-area
+
+  // 5. get map elements
+
+  // 6. get Group data
+
+  // 7. present group elements
+
 })(jQuery);
